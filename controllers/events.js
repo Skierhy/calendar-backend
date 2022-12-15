5758ec019fe742f9acdc944ecf41936549ba448e
Skierhy <Skierhy@gmail.com>
2022-12-15 17:21:55 -0600

    eliminar

--- controllers/events.js
+++ controllers/events.js
@@ -4,7 +4,7 @@ const Evento = require('../models/Evento');
 const getEventos = async (req, res = response) => {
 	// populate es para hacer la referencia al modelo Usuario y traer el nombre del usuario
 	const eventos = await Evento.find().populate('user', 'name');
-	res.status(200).json({
+	return res.status(200).json({
 		ok: true,
 		eventos,
 	});
@@ -19,13 +19,13 @@ const crearEvento = async (req, res = response) => {
 
 		const eventoGuardado = await evento.save();
 
-		res.json({
+		return res.json({
 			ok: true,
 			evento: eventoGuardado,
 		});
 	} catch (error) {
 		console.log(error);
-		res.status(500).json({
+		return res.status(500).json({
 			ok: false,
 			msg: 'Hable con el administrador',
 		});
@@ -46,7 +46,7 @@ const actualizarEvento = async (req, res = response) => {
 		const evento = await Evento.findById(eventoId);
 		// si no existe se manda un error de que no existe el evento
 		if (!evento) {
-			res.status(404).json({
+			return res.status(404).json({
 				ok: false,
 				msg: 'Evento no encontrado',
 			});
@@ -71,14 +71,14 @@ const actualizarEvento = async (req, res = response) => {
 			{ new: true }
 		);
 		// se manda el evento actualizado
-		res.json({
+		return res.json({
 			ok: true,
 			evento: eventoActualizado,
 		});
 		// si hay un error se manda un mensaje de error
 	} catch (error) {
 		console.log(error);
-		res.status(500).json({
+		return res.status(500).json({
 			ok: false,
 			msg: 'Hable con el administrador',
 		});
@@ -86,10 +86,48 @@ const actualizarEvento = async (req, res = response) => {
 };
 
 const eliminarEvento = async (req, res = response) => {
-	res.status(201).json({
-		ok: true,
-		msg: 'eliminarEvento',
-	});
+	// req significa request y res significa response
+	// request sirve para obtener información del cliente
+	// response sirve para mandar información al cliente
+	// extraer el id del evento
+	const eventoId = req.params.id;
+	// extraer el id del usuario
+	const uid = req.uid;
+	// try catch para manejar los errores de la petición
+	try {
+		// buscar el evento por el id para ver si existe uno con ese id
+		const evento = await Evento.findById(eventoId);
+		// si no existe se manda un error de que no existe el evento
+		if (!evento) {
+			return res.status(404).json({
+				ok: false,
+				msg: 'Evento no encontrado',
+			});
+		}
+		// si el usuario que quiere editar el evento no es el mismo que lo creo, se manda un error
+		if (evento.user.toString() !== uid) {
+			return res.status(401).json({
+				ok: false,
+				msg: 'No tiene privilegio de eliminar este evento',
+			});
+		}
+
+		// si todo sale bien se elimina el evento
+		await Evento.findByIdAndDelete(eventoId);
+
+		// se manda el mensaje de eliminar este event
+		return res.json({
+			ok: true,
+			msg: 'Evento eliminado',
+		});
+		// si hay un error se manda un mensaje de error
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({
+			ok: false,
+			msg: 'Hable con el administrador',
+		});
+	}
 };
 
 module.exports = {
