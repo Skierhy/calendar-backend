8f95622d3ed3ddc26d89a402ff07702857314f7c
Skierhy <Skierhy@gmail.com>
2022-12-14 20:25:57 -0600

    jwt

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,32 +1,36 @@
+// importamos la librería
 const jwt = require('jsonwebtoken');
 
-const generarJWT = ( uid, name ) => {
+// uid del usuario y usuario
+const generarJWT = (uid, name) => {
+	// retornamos una promesa
+	return new Promise((resolve, reject) => {
+		// creamos el payload
+		const payload = { uid, name };
+		// generamos el token
+		// sign sirve para firmar un token
+		// sing ( payload, secret, options, callback)
+		// options sera la expiración en 2 horas
+		// callback el cual contiene el error y el token
+		jwt.sign(
+			payload,
+			process.env.SECRET_JWT_SEED,
+			{
+				expiresIn: '2h',
+			},
+			(err, token) => {
+				if (err) {
+					console.log(err);
+					reject('No se pudo generar el token');
+				}
+				// si no hay error, resolvemos el token
+				resolve(token);
+			}
+		);
+	});
+};
 
-    return new Promise( (resolve, reject) => {
-
-        const payload = { uid, name };
-
-        jwt.sign( payload, process.env.SECRET_JWT_SEED, {
-            expiresIn: '2h'
-        }, (err, token ) => {
-
-            if ( err ){
-                console.log(err);
-                reject('No se pudo generar el token');
-            }
-
-            resolve( token );
-
-        })
-
-
-    })
-}
-
-
-
+// exportamos la función
 module.exports = {
-    generarJWT
-}
-
-
+	generarJWT,
+};
