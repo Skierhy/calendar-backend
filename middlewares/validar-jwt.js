c4b383c5cc2f7dfe0bfc734c7a02a57fd3cee440
Skierhy <Skierhy@gmail.com>
2022-12-14 21:07:06 -0600

    revalidar jwt

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -1,42 +1,42 @@
+// express para el auto completado ya en el index lo tenemos importado
 const { response } = require('express');
+// jwt para verificar el token
 const jwt = require('jsonwebtoken');
 
-const validarJWT = ( req, res = response, next ) => {
+// Validar el token
+const validarJWT = (req, res = response, next) => {
+	// x-token headers personalizados
+	const token = req.header('x-token');
 
-    // x-token headers
-    const token = req.header('x-token');
+	// Si no hay token, por ejemplo si no esta autenticado
+	if (!token) {
+		return res.status(401).json({
+			ok: false,
+			msg: 'No hay token en la petición',
+		});
+	}
+	// Si hay token, lo verificamos
+	try {
+		// Verificamos el token
+		// verify(token, seed)
+		const { uid, name } = jwt.verify(token, process.env.SECRET_JWT_SEED);
+		// Si el token es válido, agregamos el uid y el name al request
+		// para que estén disponibles en las rutas
+		// request sirve para guardar información que se puede usar en cualquier parte de la aplicación
+		req.uid = uid;
+		req.name = name;
+	} catch (error) {
+		// Si el token no es válido
+		return res.status(401).json({
+			ok: false,
+			msg: 'Token no válido',
+		});
+	}
+	// Si todo sale bien, continuamos
+	next();
+};
 
-    if ( !token ) {
-        return res.status(401).json({
-            ok: false,
-            msg: 'No hay token en la petición'
-        });
-    }
-
-    try {
-        
-        const { uid, name } = jwt.verify(
-            token,
-            process.env.SECRET_JWT_SEED
-        );
-
-        req.uid = uid;
-        req.name = name;
-
-
-    } catch (error) {
-        return res.status(401).json({
-            ok: false,
-            msg: 'Token no válido'
-        });
-    }
-
-
-
-    next();
-}
-
-
+// Exportamos el middleware
 module.exports = {
-    validarJWT
-}
+	validarJWT,
+};
